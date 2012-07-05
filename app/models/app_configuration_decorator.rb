dbd2b80a5f8c9a4074ce2c076b41df9352a59a64
John Dyer <lbrapid@gmail.com>
2012-07-05 10:28:57 -0400

    Change config

--- app/models/app_configuration_decorator.rb
+++ app/models/app_configuration_decorator.rb
@@ -3,7 +3,7 @@ Spree::AppConfiguration.class_eval do
   preference :twitter_button, :boolean, :default => true
   preference :gplus_button, :boolean, :default => true
   preference :tumblr_button, :boolean, :default => false
-  preference :pinterest_button, :boolean, :default => true
+  preference :pinterest_button, :boolean, :default => false
   preference :facebook_button, :boolean, :default => true
   preference :delicious_button, :boolean, :default => false
   preference :reddit_button, :boolean, :default => false
