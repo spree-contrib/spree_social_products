342c4392d04b6adecfc8d837401bb44de320ff58
John Dyer <lbrapid@gmail.com>
2012-07-05 09:44:59 -0400

    Added app configuration decorator

---
+++ app/models/app_configuration_decorator.rb
@@ -0,0 +1,10 @@
+Spree::AppConfiguration.class_eval do
+  # Social Sharing Preferences
+  preference :twitter_button, :boolean, :default => true
+  preference :gplus_button, :boolean, :default => true
+  preference :tumblr_button, :boolean, :default => false
+  preference :pinterest_button, :boolean, :default => true
+  preference :facebook_button, :boolean, :default => true
+  preference :delicious_button, :boolean, :default => false
+  preference :reddit_button, :boolean, :default => false
+end
