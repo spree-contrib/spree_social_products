fc6c130db468741ad6315e4c103db4d91d0a9e30
Clemens Kofler <clemens@railway.at>
2012-07-17 20:52:54 +0200

    Fix: Insert after product properties rather than replacing them
    Fixes #1

--- app/overrides/add_social_buttons_to_produts.rb
+++ app/overrides/add_social_buttons_to_produts.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => 'spree/products/show',
                      :name => 'add_social_buttons_to_products_show',
-                     :replace => "[data-hook='product_properties']",
+                     :insert_after => "[data-hook='product_properties']",
                      :partial => 'spree/shared/social_buttons')
