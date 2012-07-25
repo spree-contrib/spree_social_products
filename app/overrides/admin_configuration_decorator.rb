ea7cba0ac5830fa9f879bbab7ef3aca894d50f01
John Dyer <lbrapid@gmail.com>
2012-07-25 10:01:18 -0400

    [Fixes #2] Rename deface override to prevent conflicts with spree_social

--- app/overrides/admin_configuration_decorator.rb
+++ app/overrides/admin_configuration_decorator.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/configurations/index",
-                     :name => "add_social_providers_to_configuration_menu",
+                     :name => "add_social_products_to_configurations_menu",
                      :insert_after => "[data-hook='admin_configurations_menu']",
-                     :partial => "spree/admin/shared/configurations_menu",
+                     :partial => "spree/admin/shared/configurations_menu_social_products",
                      :disabled => false)
