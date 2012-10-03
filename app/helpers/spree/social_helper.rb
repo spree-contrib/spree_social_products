2796d76458ddf6fbc59248053f33697805a87fa8
Michael Sevestre <michael.sevestre@gmail.com>
2012-10-03 15:39:43 -0400

    Add image for open graph protocol in facebook

--- app/helpers/spree/social_helper.rb
+++ app/helpers/spree/social_helper.rb
@@ -4,13 +4,17 @@ module Spree
       return if product.images.empty?
 
       url = escape spree.product_url(product)
-      media = escape absolute_image_url(product.images.first.attachment.url)
+      media = absolute_product_image(product.images.first)
       description = escape product.name
 
       link_to("Pin It",
               "http://pinterest.com/pin/create/button/?url=#{url}&media=#{media}&description=#{description}",
               :class => "pin-it-button",
               "count-layout" => "none").html_safe
+    end
+
+    def absolute_product_image(image)
+      escape absolute_image_url(image.attachment.url)
     end
 
     private
