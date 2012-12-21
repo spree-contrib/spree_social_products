b3cad0559eecbc0147b2d5733d55372167bace6e
Kunal Chaudhari <kunalchaudhari@gmail.com>
2012-12-21 20:12:06 +0530

    fix: social sharing settings link in admin configuration sidebar for spree 1.3.0

--- app/overrides/admin_configuration_decorator.rb
+++ app/overrides/admin_configuration_decorator.rb
@@ -1,5 +1,5 @@
-Deface::Override.new(:virtual_path => "spree/admin/configurations/index",
+Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
                      :name => "add_social_products_to_configurations_menu",
-                     :insert_after => "[data-hook='admin_configurations_menu']",
+                     :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
                      :partial => "spree/admin/shared/configurations_menu_social_products",
                      :disabled => false)
