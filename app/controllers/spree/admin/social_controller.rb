c689d60e0766abffb88a35879e899b3bb4339d50
Josue David Hernandez <megadavidtor@gmail.com>
2014-08-28 17:05:28 -0500

    Change settings for facebook conections

--- app/controllers/spree/admin/social_controller.rb
+++ app/controllers/spree/admin/social_controller.rb
@@ -13,6 +13,15 @@ module Spree
           end
         end
 
+        if params[:social][:facebook]
+          Spree::Config.facebook_app_id          = params[:facebook_options][:facebook_id]
+          Spree::Config.facebook_layout          = params[:facebook_options][:facebook_layout]
+          Spree::Config.facebook_show_faces      = params[:facebook_options][:facebook_show_faces]
+          Spree::Config.facebook_verb_to_display = params[:facebook_options][:facebook_verb_to_display]
+          Spree::Config.facebook_color_scheme    = params[:facebook_options][:facebook_color_scheme]
+          Spree::Config.facebook_send_button     = params[:facebook_options][:facebook_send_button]
+    		end
+
         redirect_to edit_admin_social_path, notice: Spree.t(:social_sharing_settings_updated)
       end
     end
