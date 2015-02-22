dc87b4c70c3889b960b326e871a44cf9068b6047
Tobias Bohwalli <hi@futhr.io>
2015-02-22 09:44:11 +0100

    Decorate base helper instead.
    The current helper was not applied so better to decorate
    Spree::BaseHelper with social methods. Also the absolute_image_url was
    used in view but private so we made it public.

--- spec/helpers/spree/base_helper_decorator_spec.rb
+++ spec/helpers/spree/base_helper_decorator_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe Spree::SocialHelper, type: :helper do
+RSpec.describe Spree::BaseHelper, type: :helper do
   skip 'pin_it_button(product)'
   skip 'absolute_product_image(image)'
 end
